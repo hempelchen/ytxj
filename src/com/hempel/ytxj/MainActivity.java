2c9d145a68ab4ca4c8058f6cabf9ff62443151fc
陈红波 <hongbo.chen@renren-inc.com>
2014-06-19 17:54:17 +0800

    去掉无用代码（隐藏 title 在 XML 文件中配置）

--- src/com/hempel/ytxj/MainActivity.java
+++ src/com/hempel/ytxj/MainActivity.java
@@ -39,8 +39,6 @@ public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-//		getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN , WindowManager.LayoutParams. FLAG_FULLSCREEN);
 		setContentView(R.layout.main);
 
 		contentWebView = (WebView) findViewById(R.id.webview);
